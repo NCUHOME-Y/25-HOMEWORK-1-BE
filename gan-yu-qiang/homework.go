c68c5b1dc53011563f8ee9928b1f174168f320b9
gyq <2127069700@qq.com>
2025-09-28 23:09:18 +0800

    feat:添加乘法功能

--- gan-yu-qiang/homework.go
+++ gan-yu-qiang/homework.go
@@ -12,9 +12,18 @@ func (count1 Complex) Add(count2 Complex) Complex {
 	}
 }
 
-func (count1 Complex) Sub(count2 Complex) Complex {
+func (count1 Complex) Subtract(count2 Complex) Complex {
 	return Complex{
 		Real: count1.Real - count1.Real,
 		Imag: count1.Imag - count2.Imag,
+	}
+}
+
+func (count1 Complex) Multiply(count2 Complex) Complex {
+	realPart := c1.Real*c2.Real - c1.Imag*c2.Imag
+	imagPart := c1.Real*c2.Imag + c1.Imag*c2.Real
+	return Complex{
+		Real: realPart,
+		Imag: imagPart,
 	}
 }
