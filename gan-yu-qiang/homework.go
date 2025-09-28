d05b0a3ba256eac84e52a65313294cdeba0049db
gyq <2127069700@qq.com>
2025-09-28 22:56:57 +0800

    feat:加减法功能

--- gan-yu-qiang/homework.go
+++ gan-yu-qiang/homework.go
@@ -1,1 +1,20 @@
 package main
+
+type Complex struct {
+	Real float64
+	Imag float64
+}
+
+func (count1 Complex) Add(count2 Complex) Complex {
+	return Complex{
+		Real: count1.Real + count2.Real,
+		Imag: count1.Imag + count2.Imag,
+	}
+}
+
+func (count1 Complex) Sub(count2 Complex) Complex {
+	return Complex{
+		Real: count1.Real - count1.Real,
+		Imag: count1.Imag - count2.Imag,
+	}
+}
