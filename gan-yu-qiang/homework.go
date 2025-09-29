baa3ffbc2f7dea97f30525d9e1623e06a6171307
gyq <2127069700@qq.com>
2025-09-29 08:53:08 +0800

    feat: 添加除法并讨论分母为零的情况

--- gan-yu-qiang/homework.go
+++ gan-yu-qiang/homework.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 type Complex struct {
 	Real float64
 	Imag float64
@@ -26,4 +30,20 @@ func (count1 Complex) Multiply(count2 Complex) Complex {
 		Real: realPart,
 		Imag: imagPart,
 	}
+}
+
+func (count1 Complex) Divide(count2 Complex) (Complex, error) {
+	mother := count2.Real*count2.Real + count2.Imag*count2.Imag
+	if mother == 0 {
+		var errString string = "错误：分母为零"
+		return Complex{}, errors.New(errString)
+	} else {
+		realSon := count1.Real*count2.Real + count1.Imag*count2.Imag
+		imagSon := count1.Imag*count2.Real - count1.Real*count2.Imag
+		return Complex{
+			Real: realSon / mother,
+			Imag: imagSon / mother,
+		}, nil
+	}
+
 }
