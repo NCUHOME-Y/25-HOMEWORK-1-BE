b67a132da612a23287af436bb210dd0b98563a81
mingyuu <dengzb714@163.com>
2025-10-11 17:23:37 +0800

    fix

--- deng-zeng-bo/main.go
+++ deng-zeng-bo/main.go
@@ -37,7 +37,10 @@ func (c Complex) Mul(other Complex) Complex {
 
 // Div 除法：(a+bi)/(c+di) = [(ac + bd)+(bc - ad)i] / (c² + d²)
 func (c Complex) Div(other Complex) Complex {
-	denominator := other.Real*other.Real + other.Imag*other.Imag//定义分母,简化了代码
+	denominator := other.Real*other.Real + other.Imag*other.Imag // 定义分母,简化了代码
+	if denominator == 0 {
+		panic("除数不能为零")
+	}
 	return Complex{
 		Real: (c.Real*other.Real + c.Imag*other.Imag) / denominator,
 		Imag: (c.Imag*other.Real - c.Real*other.Imag) / denominator,
@@ -53,32 +56,34 @@ func (c Complex) Modulus() float64 {
 // 当使用 fmt 包的打印函数（如 fmt.Println、fmt.Printf 等）输出 Complex 类型的变量时，会自动调用这个方法来获取字符串表示形式
 // 统一格式化输出，简化了代码，(本段学习了ai的处理办法)
 func (c Complex) String() string {
-	if c.Imag >= 0 {
+	if c.Imag == 0 {
+		return fmt.Sprintf("%.2f",c.Real)
+	}
+	if c.Imag > 0 {
 		return fmt.Sprintf("%.2f+%.2fi", c.Real, c.Imag)
 	}
 	return fmt.Sprintf("%.2f%.2fi", c.Real, c.Imag)
 }
 
 func main() {
-	var c1,c2 Complex
+	var c1, c2 Complex
 	fmt.Println("请输入两个数的实部和虚部")
-	fmt.Scan(&c1.Real,&c1.Imag,&c2.Real,&c2.Imag)
+	fmt.Scan(&c1.Real, &c1.Imag, &c2.Real, &c2.Imag)
 
-	fmt.Println("c1 =", c1)
-	fmt.Println("c2 =", c2)
-	//和
+
+	// 和
 	sum := c1.Add(c2)
 	fmt.Println("c1 + c2 =", sum)
-	//差
+	// 差
 	diff := c1.Sub(c2)
 	fmt.Println("c1 - c2 =", diff)
-	//积
+	// 积
 	product := c1.Mul(c2)
 	fmt.Println("c1 * c2 =", product)
-	//商
+	// 商
 	quotient := c1.Div(c2)
 	fmt.Println("c1 / c2 =", quotient)
-	//模
+	// 模
 	modulus := c1.Modulus()
 	fmt.Printf("|c1| = %.2f\n", modulus)
 }
